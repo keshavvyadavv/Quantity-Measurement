f20bf864f80dd1adf3bc65ebbe4ce29020e26739
keshavvyadavv <keshavyadavv21@gmail.com>
2026-02-23 16:27:11 +0530

    [Keshav Yadav] : Added VolumeUnit.java

---
+++ src/main/java/com/apps/quantitymeasurement/domain/VolumeUnit.java
@@ -0,0 +1,33 @@
+package com.apps.quantitymeasurement.domain;
+
+public enum VolumeUnit implements IMeasurable {
+
+    LITRE(1.0),
+    MILLILITRE(0.001),
+    GALLON(3.78541);
+
+    private final double conversionFactor; 
+    
+    VolumeUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
+    public double toBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    @Override
+    public double fromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
+}
