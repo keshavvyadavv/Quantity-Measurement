4a9144a5d294f42751581a7f3987bcc086ee4249
keshavvyadavv <keshavyadavv21@gmail.com>
2026-02-23 19:52:15 +0530

    [Keshav Yadav] : Modified IMeasurable.java

--- src/main/java/com/apps/quantitymeasurement/domain/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/domain/IMeasurable.java
@@ -4,5 +4,14 @@ public interface IMeasurable {
 
     double toBase(double value);
     double fromBase(double baseValue);
-    
+
+    SupportsArithmetic supportsArithmetic = () -> true;
+
+    default boolean supportsArithmetic() {
+        return supportsArithmetic.isSupported();
+    }
+
+    default void validateOperationSupport(String operation) {
+        
+    }
 }
