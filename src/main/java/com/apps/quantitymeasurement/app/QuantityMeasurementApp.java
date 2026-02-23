e5682b4b167740527c5dc54013e8c8864bdb1061
keshavvyadavv <keshavyadavv21@gmail.com>
2026-02-23 11:42:28 +0530

    [Keshav Yadav] : Added QuantityMeasurementApp.java

--- src/main/java/com/apps/quantitymeasurement/app/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/app/QuantityMeasurementApp.java
@@ -1,46 +1,36 @@
 package com.apps.quantitymeasurement.app;
 import java.util.Scanner;
+
+import com.apps.quantitymeasurement.domain.Length;
 import com.apps.quantitymeasurement.domain.LengthUnit;
 import com.apps.quantitymeasurement.domain.Quantity;
 public class QuantityMeasurementApp {
 	
-	public static boolean checkEquality(double value1 , LengthUnit unit1, double value2 , LengthUnit unit2) {
-		Quantity q1 = new Quantity(value1 , unit1);
-		Quantity q2 = new Quantity(value2 , unit2);
-		
-		return q1.equals(q2);
-	}
+	public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+        return length1.equals(length2);
+    }
+
+    public static void demonstrateLengthComparison(double value1, Length.LengthUnit unit1,double value2, Length.LengthUnit unit2) {
+
+        Length l1 = new Length(value1, unit1);
+        Length l2 = new Length(value2, unit2);
+
+        System.out.println("Input: " + l1 + " and " + l2);
+        System.out.println("Output: Equal (" + demonstrateLengthEquality(l1, l2) + ")");
+        System.out.println();
+    }
 	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		try {
+
+			demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,3.0, Length.LengthUnit.FEET);
+
+			demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,36.0, Length.LengthUnit.INCHES);
 			
-			System.out.println("Enter first value:");
-            double value1 = Double.parseDouble(scanner.nextLine());
+			demonstrateLengthComparison(2.0, Length.LengthUnit.YARDS,2.0, Length.LengthUnit.YARDS);
+			
+			demonstrateLengthComparison(2.0, Length.LengthUnit.CENTIMETERS,2.0, Length.LengthUnit.CENTIMETERS);
+			
+			demonstrateLengthComparison(1.0, Length.LengthUnit.CENTIMETERS,0.393701, Length.LengthUnit.INCHES);
 
-            System.out.println("Enter first unit (feet/inch):");
-            LengthUnit unit1 = LengthUnit.fromString(scanner.nextLine());
-
-            System.out.println("Enter second value:");
-            double value2 = Double.parseDouble(scanner.nextLine());
-
-            System.out.println("Enter second unit (feet/inch):");
-            LengthUnit unit2 = LengthUnit.fromString(scanner.nextLine());
-
-            Quantity q1 = new Quantity(value1, unit1);
-            Quantity q2 = new Quantity(value2, unit2);
-
-            boolean result = q1.equals(q2);
-
-            System.out.println();
-            System.out.println("Input: " + q1 + " and " + q2);
-            System.out.println("Output: Equal (" + result + ")");
-		} catch (NumberFormatException e) {
-            System.out.println("Invalid number entered.");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        } finally {
-            scanner.close();
-        }
 	}
 }
