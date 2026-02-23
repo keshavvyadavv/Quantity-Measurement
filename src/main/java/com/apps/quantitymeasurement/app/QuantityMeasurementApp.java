7370cf7f5d76e8246c6561aab2e3cef840bff4a6
keshavvyadavv <keshavyadavv21@gmail.com>
2026-02-23 19:53:13 +0530

    [Keshav Yadav] : Modified QuantityMeasurementApp.java

--- src/main/java/com/apps/quantitymeasurement/app/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/app/QuantityMeasurementApp.java
@@ -1,29 +1,25 @@
 package com.apps.quantitymeasurement.app;
-import java.util.Scanner;
+
 
 import com.apps.quantitymeasurement.domain.LengthUnit;
 import com.apps.quantitymeasurement.domain.Quantity;
+import com.apps.quantitymeasurement.domain.TemperatureUnit;
 import com.apps.quantitymeasurement.domain.VolumeUnit;
 import com.apps.quantitymeasurement.domain.WeightUnit;
 public class QuantityMeasurementApp {
 	
 	public static void main(String[] args) {
 		
-        System.out.println(new Quantity<>(10.0, LengthUnit.FEET).subtract(new Quantity<>(6.0, LengthUnit.INCHES)));
+        System.out.println( new Quantity<>(0.0, TemperatureUnit.CELSIUS).equals(new Quantity<>(32.0, TemperatureUnit.FAHRENHEIT)));
 
-        System.out.println(new Quantity<>(5.0, VolumeUnit.LITRE).subtract(new Quantity<>(500.0, VolumeUnit.MILLILITRE)));
-        System.out.println(new Quantity<>(10.0, LengthUnit.FEET).divide(new Quantity<>(2.0, LengthUnit.FEET)));
+        System.out.println( new Quantity<>(100.0, TemperatureUnit.CELSIUS).convertTo(TemperatureUnit.FAHRENHEIT));
 
-        System.out.println(new Quantity<>(24.0, LengthUnit.INCHES).divide(new Quantity<>(2.0, LengthUnit.FEET)) );
-
-       
         try {
-            System.out.println(
-                    new Quantity<>(10.0, LengthUnit.FEET)
-                            .divide(new Quantity<>(0.0, LengthUnit.FEET))
-            );
+            new Quantity<>(100.0, TemperatureUnit.CELSIUS).add(new Quantity<>(50.0, TemperatureUnit.CELSIUS));
+        
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+        	
+            System.out.println(e.getMessage());
         }
         
 	}
