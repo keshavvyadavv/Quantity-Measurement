98becd933efb1ce45669cf6027c107021b57b7b6
keshav <keshavyadavv21@gmail.com>
2026-04-05 11:57:49 +0530

    Update OAuth2SuccessHandler.java

--- microservice-architecture/security-service/src/main/java/com/security/security/OAuth2SuccessHandler.java
+++ microservice-architecture/security-service/src/main/java/com/security/security/OAuth2SuccessHandler.java
@@ -6,6 +6,7 @@ import com.security.service.JwtService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -20,6 +21,9 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final UserRepository repository;
     private final JwtService jwtService;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
@@ -29,11 +33,9 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
         String emailTemp = oAuth2User.getAttribute("email");
-
         if (emailTemp == null) {
             emailTemp = oAuth2User.getAttribute("login") + "@github.com";
         }
-
         final String email = emailTemp;
 
         User user = repository.findByEmail(email)
@@ -48,9 +50,6 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
         String token = jwtService.generateToken(user);
 
-        response.setContentType("application/json");
-        response.getWriter().write(
-                "{\"token\": \"" + token + "\", \"message\": \"OAuth login successful\"}"
-        );
+        response.sendRedirect(frontendUrl + "/dashboard?token=" + token);
     }
 }
