41da6261444cfdaafa2b80dcdee678e003c142b8
keshav <keshavyadavv21@gmail.com>
2026-04-06 12:39:14 +0530

    Update SecurityConfig.java

--- microservice-architecture/security-service/src/main/java/com/security/config/SecurityConfig.java
+++ microservice-architecture/security-service/src/main/java/com/security/config/SecurityConfig.java
@@ -41,8 +41,7 @@ public class SecurityConfig {
                     "/swagger-ui/**",
                     "/actuator/**",
                     "/swagger-ui.html",
-                    "/v3/api-docs/**",
-                    "/**"
+                    "/v3/api-docs/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
